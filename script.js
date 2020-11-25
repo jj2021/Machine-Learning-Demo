99df80796c79f126e5e6ea791fc60ec67a5fb785
jj2021 <james.jenkins2@marist.edu>
2020-11-24 22:11:33 -0500

    Connect neurons in the diagram

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('diagram');
 const ctx = canvas.getContext('2d');
+const radius = 50;
 canvas.width = 800;
 canvas.height = 500;
 
@@ -7,6 +8,7 @@ canvas.height = 500;
  * Draw the diagram of the neural network onto the canvas
  */
 function animate() {
+  // Draw neurons
   drawNeuron(100,100,'x1');
   drawNeuron(100,300,'x2');
   drawNeuron(250,400,'b1');
@@ -14,6 +16,54 @@ function animate() {
   drawNeuron(400,300,'h2');
   drawNeuron(550,400,'b2');
   drawNeuron(700,200,'y1');
+
+  // Connect the neurons
+
+  // Connect x1 to h1
+  connectNeurons(100,100,400,100);
+  // Connect x2 to h2
+  connectNeurons(100,300,400,300);
+  //Connect x1 to h2
+  connectNeurons(100,100,400,300);
+  // Connect x2 to h1
+  connectNeurons(100,300,400,100);
+  // Connect b1 to h1
+  connectNeurons(250,400,400,100);
+  // Connect b1 to h2
+  connectNeurons(250,400,400,300);
+  // Connect h1 to y1
+  connectNeurons(400,100,700,200);
+  // Connect h2 to y1
+  connectNeurons(400,300,700,200);
+  // Connect b2 to y1
+  connectNeurons(550,400,700,200);
+}
+
+/**
+ * Connect two neurons in the diagram
+ * @param {number} x1 - X coordinate of neuron 1
+ * @param {number} y1 - Y coordinate of neuron 1
+ * @param {number} x2 - X coordinate of neuron 2
+ * @param {number} y2 - Y coordinate of neuron 2
+ */
+function connectNeurons(x1,y1,x2,y2) {
+  // Calculate angle of line from neuron 1 to neuron 2
+  // and set the start point 
+  let angle = Math.atan2(y2-y1,x2-x1);
+  let startX = radius * Math.cos(angle) + x1;
+  let startY = radius * Math.sin(angle) + y1;
+
+  // Calculate angle from neuron 2 to neuron 1
+  // and set the destination point
+  angle = Math.atan2(y1-y2, x1-x2);
+  let targetX = radius * Math.cos(angle) + x2;
+  let targetY = radius * Math.sin(angle) + y2;
+
+  // Draw the line
+  ctx.beginPath();
+  ctx.moveTo(startX,startY);
+  ctx.lineTo(targetX,targetY);
+  ctx.stroke();
 }
 
 /**
@@ -25,7 +75,7 @@ function animate() {
 function drawNeuron(x, y, lable) {
   // Draw the neuron
   ctx.beginPath();
-  ctx.arc(x, y, 50, 0, 2*Math.PI);
+  ctx.arc(x, y, radius, 0, 2*Math.PI);
   ctx.stroke();
 
   // Draw neuron lable
